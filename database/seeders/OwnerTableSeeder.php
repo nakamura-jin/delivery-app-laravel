1f77231e91f434728595f319865cb4ac9e8fc247
中村　迅 <shu.kyu0224@icloud.com>
2021-12-08 19:11:46 +0900

    owner用seeder追加

---
+++ database/seeders/OwnerTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Owner;
+
+class OwnerTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'id' => 1,
+            'name' => 'admin',
+            'email' => 'admin@test.com',
+            'uid' => Hash::make('7OuoFhraJtblFlhlb2DfP4XcK5i2'),
+            'role_id' => 1
+        ];
+        $item = new Owner;
+        $item->fill($param)->save();
+
+        $param = [
+            'id' => 2,
+            'name' => 'member',
+            'email' => 'member@test.com',
+            'uid' => Hash::make('BVrf7zWUESOsipuWHbNE4lhMnhE'),
+            'role_id' => 2
+        ];
+        $item = new Owner;
+        $item->fill($param)->save();
+    }
+}
--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -17,20 +17,10 @@ class UserTableSeeder extends Seeder
     {
         $param = [
             'id' => 1,
-            'name' => 'admin',
-            'email' => 'admin@test.com',
+            'name' => 'user',
+            'email' => 'user@test.com',
             'uid' => Hash::make('7OuoFhraJtblFlhlb2DfP4XcK5i2'),
             'role_id' => 1
-        ];
-        $item = new User;
-        $item->fill($param)->save();
-
-        $param = [
-            'id' => 2,
-            'name' => 'member',
-            'email' => 'member@test.com',
-            'uid' => Hash::make('BVrf7zWUESOsipuWHbNE4lhMnhE'),
-            'role_id' => 2
         ];
         $item = new User;
         $item->fill($param)->save();
